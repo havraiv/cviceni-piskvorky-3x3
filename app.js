32ce05a4397ab4b95e9cf42b3554be87c64fcb79
Iva Havranová <havranova.i@seznam.cz>
2023-04-13 21:23:20 +0200

    findWinner aplikováno na první hrací pole

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
-import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4' // Tento řádek vám umožňuje používat funkci findWinner od jiného programátora. Více se o zápisu s import dozvíte v pozdějších lekcích.
+import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4';
 
-// Kód níže je pouze ukázka použití funkce findWinner. Následující řádky smažte.
+const hra = document.querySelector('.hra');
+const herniPole = document.querySelector('.herni-pole');
+const policko = herniPole.querySelectorAll('.policko');
+const vysledek = document.querySelector('.vysledek');
 
-// Ukázka použití pro rozehranou hru 3x3, kde začínající hráč kolečko hrál všechny své tři tady do prvního sloupečku a druhý hráč křížek všechny své dva tahy do druhého sloupečku.
-const ukazkoveHerniPole = ['o', 'x', '_', 'o', 'x', '_', 'o', '_', '_']
-// Pro lepší představu stejné pole lze zapsat i takto:
-/*
-const ukazkoveHerniPole = [
-	'o', 'x', '_',
-	'o', 'x', '_',
-	'o', '_', '_',
-]
-*/
-const vitez = findWinner(ukazkoveHerniPole)
-console.log(`Vyhrál hráč se symbolem ${vitez}.`) // 'Vyhrál hráč se symbolem o.'
+const vsechnaPolicka = Array.from(policko);
+
+const obsahPolicek = vsechnaPolicka.map((symbol) => {
+  if (symbol.classList.contains('kolecko')) {
+    return 'o';
+  }
+  if (symbol.classList.contains('krizek')) {
+    return 'x';
+  }
+  if (symbol.classList.contains('nic')) {
+    return '_';
+  }
+});
+
+const vitez = findWinner(obsahPolicek);
+
+if (vitez === 'o') {
+  vysledek.textContent = 'Vyhrálo kolečko!';
+}
+if (vitez === 'x') {
+  vysledek.textContent = 'Vyhrál křížek!';
+}
+if (vitez === 'tie') {
+  vysledek.textContent = 'Remíza!';
+}
+if (vitez === null) {
+  vysledek.textContent = 'Hra ještě probíhá.';
+}
