b08353ceb79acf3cd95d2be50feea6a778b9e775
Iva Havranová <havranova.i@seznam.cz>
2023-04-13 22:06:25 +0200

    Bonus hotov

--- app.js
+++ app.js
@@ -1,35 +1,36 @@
 import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4';
 
-const hra = document.querySelector('.hra');
-const herniPole = document.querySelector('.herni-pole');
-const policko = herniPole.querySelectorAll('.policko');
-const vysledek = document.querySelector('.vysledek');
+const hra = document.querySelectorAll('.hra');
 
-const vsechnaPolicka = Array.from(policko);
+hra.forEach((herniPole) => {
+  const policko = herniPole.querySelectorAll('.policko');
+  const vysledek = herniPole.querySelector('.vysledek');
+  const vsechnaPolicka = Array.from(policko);
 
-const obsahPolicek = vsechnaPolicka.map((symbol) => {
-  if (symbol.classList.contains('kolecko')) {
-    return 'o';
+  const obsahPolicek = vsechnaPolicka.map((symbol) => {
+    if (symbol.classList.contains('kolecko')) {
+      return 'o';
+    }
+    if (symbol.classList.contains('krizek')) {
+      return 'x';
+    }
+    if (symbol.classList.contains('nic')) {
+      return '_';
+    }
+  });
+
+  const vitez = findWinner(obsahPolicek);
+
+  if (vitez === 'o') {
+    vysledek.textContent = 'Vyhrálo kolečko!';
   }
-  if (symbol.classList.contains('krizek')) {
-    return 'x';
+  if (vitez === 'x') {
+    vysledek.textContent = 'Vyhrál křížek!';
   }
-  if (symbol.classList.contains('nic')) {
-    return '_';
+  if (vitez === 'tie') {
+    vysledek.textContent = 'Remíza!';
+  }
+  if (vitez === null) {
+    vysledek.textContent = 'Hra ještě probíhá.';
   }
 });
-
-const vitez = findWinner(obsahPolicek);
-
-if (vitez === 'o') {
-  vysledek.textContent = 'Vyhrálo kolečko!';
-}
-if (vitez === 'x') {
-  vysledek.textContent = 'Vyhrál křížek!';
-}
-if (vitez === 'tie') {
-  vysledek.textContent = 'Remíza!';
-}
-if (vitez === null) {
-  vysledek.textContent = 'Hra ještě probíhá.';
-}
